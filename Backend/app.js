edcdf03baf39362290c300d3c12fc92793d8ea0b
Alisha <aggarwalalisha071@gmail.com>
2026-01-22 12:26:42 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,23 @@ const app=express();
 const dotenv=require('dotenv')
 const cors=require('cors');
 dotenv.config();
+const connectToDb=require("./db/db")
+const userRoutes=require("./routes/user.route")
+
+connectToDb()
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
 
 });
 
-module.exports=app
+app.use("/users",userRoutes);
+
+
+module.exports=app;
 
